01617098b01dca2e361e7fa6d79e6ed04b8020b8
Elizaveta <darck.mo@yandex.ru>
2021-02-24 20:25:24 +0300

    commit 1

---
+++ src/Task1/Task1.java
@@ -0,0 +1,11 @@
+package Task1;
+
+public class Task1 {
+    public static int convert (int min) {
+        return min * 60;
+    }
+    public static int points(int two, int three){
+        return two*2 + three*3;
+    }
+    //System.out.println(); //Принт в консоль
+}
